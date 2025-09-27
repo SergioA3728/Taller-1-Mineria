5eff81bab958a6002c9944c9dd0e4b478d0cd4b0
Sergio Avila <sergioandres3728@hotmail.com>
2025-09-26 22:57:33 -0500

    agregue varibles

--- index.py
+++ index.py
@@ -5,3 +5,16 @@ import random
 #configs
 n = 500
 np.ramdom.seed(42)
+#variales
+Edad= np.ramdom.ramdint(16,30,n)
+Genero= np.ramdon.choice (['M','F'],n)
+Lugar_origen= np.ramdon.choice (['soledad','Sabana larga','Sabana Grande','Baranoa','Barranquilla'],n)
+PM_bachiderato= np.round(np.ramdon.uniform(2.0,5.0,n),2)
+ICFES= np.round(np.ramdon.uniform(200,500,n),2)
+NT_Semestre1= np.round(np.ramdon.uniform(2.0,5.0,n),2)
+Estrato= np.ramdon.choice ([1,2,3,4,5,6],n)
+Beca= np.ramdon.choice (['Si','No'],n)
+Icetex= np.ramdon.choice (['Si','No'],n)
+Desercion= np.where((PM_bachiderato<3.0)|(NT_Semestre1<3.0),
+                    np.ramdon.choice (['Si','No'],n,p=[0.7,0.3]), 
+                    np.ramdon.choice (['Si','No'],n,p=[0.2,0.8]))
